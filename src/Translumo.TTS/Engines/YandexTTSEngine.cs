3cd56e4f7265d5f2af168534503d77e236c9d53b
Ivan <ivan@gubanov.cf>
2023-10-22 12:43:15 +0300

    draft implementation yandex ttx

---
+++ src/Translumo.TTS/Engines/YandexTTSEngine.cs
@@ -0,0 +1,95 @@
+﻿using NAudio.Wave;
+
+namespace Translumo.TTS.Engines;
+
+public class YandexTTSEngine : ITTSEngine
+{
+    private readonly string _langCode;
+    private string _voice;
+    private CancellationTokenSource _tokenSource;
+    private readonly DirectSoundOut _naudioPlayer;
+    private readonly string _emotion = "neutral"; // neutral|good|evil
+    private const float _speed = 1.0f;
+    private const string _format = "mp3"; // ogg | mp3 | wav
+    private const string _key = "1"; // yandex doesnt validate key, its can have any not empty value
+
+    public YandexTTSEngine(string langCode)
+    {
+        _langCode = langCode;
+        _voice = GetVoices().First();
+        _naudioPlayer = new DirectSoundOut();
+        _tokenSource = new CancellationTokenSource();
+    }
+
+    public void SpeechText(string text)
+    {
+        _tokenSource.Cancel();
+        _tokenSource = new CancellationTokenSource();
+        var currentToken = _tokenSource.Token;
+        Task.Run(() => SpeechTextInternalAsync(text, currentToken));
+    }
+
+    private async Task SpeechTextInternalAsync(string text, CancellationToken token)
+    {
+        using var stream = await RequestAudioAsync(text, token);
+        using var audioFileReader = new Mp3FileReader(stream);
+
+        _naudioPlayer.Stop();
+        _naudioPlayer.Init(audioFileReader);
+        _naudioPlayer.Play();
+
+        var duration = Convert.ToInt32(Math.Round(audioFileReader.TotalTime.TotalMilliseconds, MidpointRounding.ToPositiveInfinity));
+        Task.Delay(duration).Wait(token);
+    }
+
+    private async Task<Stream> RequestAudioAsync(string text, CancellationToken token)
+    {
+        var httpClient = new HttpClient();
+        var httpResponse = await httpClient.GetAsync(BuildUrl(text), token).ConfigureAwait(false);
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            return await httpResponse.Content.ReadAsStreamAsync(token).ConfigureAwait(false);
+        }
+
+        var error = await httpResponse.Content.ReadAsStringAsync(token);
+        throw new InvalidOperationException($"Failed to get sound: '{httpResponse.StatusCode}, {error}'");
+    }
+
+    private string BuildUrl(string text)
+    {
+        var encodedText = Uri.EscapeDataString(text);
+        return $"https://tts.voicetech.yandex.net/generate?text={encodedText}&lang={_langCode}&key={_key}&speaker={_voice}&format={_format}&speed={_speed:#.##}&emotion={_emotion}&quality=hi";
+    }
+
+    public void SetVoice(string voice) =>
+    _voice = GetVoices().FirstOrDefault(x => x.Equals(voice, StringComparison.OrdinalIgnoreCase))
+        ?? throw new InvalidDataException($"Voice '{voice}' doesnt support '{_langCode}' language ");
+
+    public string[] GetVoices() =>
+        SupportedVoices().GetValueOrDefault(_langCode)
+        ?? throw new InvalidDataException($"{nameof(YandexTTSEngine)} doesnt support '{_langCode}' language");
+
+    public void Dispose()
+    {
+        _tokenSource.Cancel();
+        _naudioPlayer.Stop();
+        _naudioPlayer.Dispose();
+    }
+
+    private static Dictionary<string, string[]> SupportedVoices()
+    {
+       
+        var voices = new Dictionary<string, string[]>()
+        {
+            { "ru-RU", new[] {  "jane", "oksana", "alyss", "omazh", "ermil", "zahar" }},
+            //{ "uz-UZ", new[] { "nigora" }},
+            //{ "de-DE", new[] { "lea" }},
+            //{ "en-US", new[] { "john" }},
+            //{ "kk-KK", new[] { "amira", "madi" }},
+            //{ "ALL: 'en': 'en_US','ru_RU','uk_UK ,'tr_TR',", new[] {  "jane", "oksana", "alyss", "omazh", "ermil", "zahar" }}, //"alena","filipp","madirus",
+
+            //{ "ru-RU", new [] {"alyss", "dude", "erkanyavas", "ermil", "ermilov", "jane", "kolya", "kostya", "levitan", "nastya", "nick", "oksana", "omazh", "sasha", "silaerkan", "smoky", "tanya", "voicesearch", "zahar", "zhenya", "zombie" } },
+        };
+        return voices;
+    }
+}
--- src/Translumo.TTS/TtsFactory.cs
+++ src/Translumo.TTS/TtsFactory.cs
@@ -25,6 +25,7 @@ namespace Translumo.TTS
                 TTSEngines.None => new NoneTTSEngine(),
                 TTSEngines.WindowsTTS => new WindowsTTSEngine(GetLangCode(ttsConfiguration)),
                 TTSEngines.SileroTTS => new SileroTTSEngine(_pythonEngine, GetLangCode(ttsConfiguration)),
+                TTSEngines.YandexTTS => new YandexTTSEngine(GetLangCode(ttsConfiguration)),
                 _ => throw new NotSupportedException()
             };
 
